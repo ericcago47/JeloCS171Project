f15efe73a0c11fcc4b2d2ff499934789d703799a
Elizabeth Joseph <itselizabethjoseph@gmail.com>
2017-11-09 23:46:28 -0800

    implemented logic to mark possible threats, safe squares, and generate neighbors

--- MyAI.py
+++ MyAI.py
@@ -18,124 +18,88 @@
 # ======================================================================
 
 from Agent import Agent
-import random
+from collections import defaultdict 
+import inspect 
 
 class MyAI ( Agent ):
 
     def __init__ ( self ):
-	    # ======================================================================
-	    # YOUR CODE BEGINS
-	    # ======================================================================
-	        
-        self._direction = 'right'
-		self._currentLocation = (0,0)
-		self._X = 0
-		self._Y = 0
-		self._prevAction = None
-		self._hasArrow = True
-		self._hasGold = False
-		self._rowLen = None
-		self._colLen = None
-		self._deadWumpus = False
-		# Game state has min 4x4
-		self._gameState = [[['@','v'],[],[],[]],[[],[],[],[]],[[],[],[],[]],[[],[],[],[]]]	
-		# @ = Agent
-		# v = Visited
-		# br = Breeze
-		# bu = Bump
-		# s = Stench
-		# p = Pit
-		# go = Gold
-		# gl = Glitter
-		# w = Wumpus
-		# pp = Possible Pit
-		# pw = Possible Wumpus
-
+        # ======================================================================
+        # YOUR CODE BEGINS
+        # ======================================================================
+		self.x = 1 
+		self.y = 1 
+		self.hasArrow = False 
+		self.rowLen 
+		self.colLen 
+		self.wumpusDead = False 
+		self.hasGold = False 
+		self.gameNodes = {(1,1): [(2,1), (1,2)]} #graph holding visited nodes as keys and their neighboring nodes as values 
+		self.gameStates = defaultdict(list) 
+		self.gameStates[(1,1)].append('S') #graph holding nodes as keys and their threat state as values 
+		self.gameMoves = [] 
+	
+        pass
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
+	def generateNeighbors(int x, int y): 
+		neighbors = [] 
+		if x >= 2 
+			neighbors.append((x-1, y)) #generate the left neighbor 
+        neighbors.append((x+1, y)) #generate the right neighbor
+		if y >= 2
+			neighbors.append((x, y-1)) #generate the meighbor below
+		neighbors.append((x, y+1)) #generate the neighbor above
+		return neighbors 
+
+	def updateGameNodes():
+		if (self.x, self.y) not in self.gameNodes: 
+			self.gameNodes[(self.x, self.y)].append(generateNeighbors(self.x,self.y))
+
+	def updatePossibleThreat(threat:str):
+		#possiblePits = generateNeighbors(self.x,self.y) 
+		for node in self.gameNodes[(self.x, self.y)]:
+			if node not in self.gameNodes and ('N' + threat) not in self.gameStates[node]: : #check that the node isnt visited because that means it is safe  
+				self.gameStates[node].append('P'+threat)  
+
+	def notThreat(threat: str, stench: bool, breeze: bool):
+		#implement inspect stuff 
+		if breeze == False or stench == False:
+			neighbors = generateNeighbors(self.x,self.y) 
+			for node in neighbors: 
+				if node in self.gameStates.keys(): #check dictionary syntax --if node is a key in gameStates  
+					if ('P' + threat) in self.gameStates[node]:
+						self.gameStates[node].remove('P'+threat) 
+						self.gameStates[node].append('N' + threat)
+						if threat == 'W':                
+							if 'NP' in self.gameStates[node]: 
+								self.gameStates[node] = ['S']
+						elif 'NW' in self.gameState[node]: 
+								self.gameStates[node] = ['S']
+
+	def markSafe(): 
+		if self.gameStates[(self.x,self.y)] == ['S']: 
+			neighbors = generateNeighbors(self.x,self.y) 
+			for node in neighbors: 
+				self.gameStates[node] = ['S']
+		
+	def generateMoves(): 
+		
+				
 
     def getAction( self, stench, breeze, glitter, bump, scream ):
         # ======================================================================
         # YOUR CODE BEGINS
         # ======================================================================
-		nextMove = Agent.Action.CLIMB
-		validMoves = __actions
-		# random.randrange ( len ( self.__actions )
-		# Updating GameState based on percepts
-		if stench:
-		    if 's' not in self._gameState[self._X][self._Y]:
-		    	self._gameState[self._X][self._Y].append('s')		
-		    	possibleThreat(self,'w')
-		if breeze:
-		    if 'br' not in self._gameState[self._X][self._Y]:
-				self._gameState[self._X][self._Y].append('br')
-				possibleThreat(self,'p')
-			if self._currentLocation == (0,0):
-				#TODO: Change in future; right now, climb out automatically
-				nextMove = Agent.Action.CLIMB 
-				self._prevAction = nextMove
-				return nextMove
-		if glitter:
-			if 'gl' not in self._gameState[self._X][self._Y]:
-				self._gameState[self._X][self._Y].append('gl')
-	    	self._hasGold = True		
-		   	nextMove = Agent.Action.GRAB
-		    self._prevAction = nextMove
-		    return nextMove
-		if bump:
-		    #update rowLen or cowLen here
-		    if self._direction == 'right':
-				self._colLen = self._Y + 1
-		    elif self._direction == 'up':
-				self._rowLen = self._X + 1
-		if scream:
-		    self._deadWumpus = True
-		else:
-		    pass
-	   	    #randomly choose direction
-		#if we know we're in first row, we know not to turn right
-
-		if nextMove == Agent.Action.SHOOT:
-		    self._hasArrow = False
-
-		self._prevAction = nextMove
-	        return nextMove
+         
+        return Agent.Action.CLIMB
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
     
     # ======================================================================
     # YOUR CODE BEGINS
-    __actions = [
-        Agent.Action.TURN_LEFT,
-        Agent.Action.TURN_RIGHT,
-        Agent.Action.FORWARD,
-        Agent.Action.CLIMB,
-        Agent.Action.SHOOT,
-        Agent.Action.GRAB
-    ]
-    def validActions(self):
-        pass
-
-    def possibleThreat(self,threat_type):
-	# Check past move / if square has been visited
-	threat = 'p' + threat_type
-	if pw == True:
-            if self._X != (self._rowLen - 1) and ('v' not in self._gameState[self._X+1][self._Y]):
-		#TODO: if row's not in gameState, add it! Same w/ columns
-		if len(self._gameState) == (self._X + 1):
-		    self._gameState.append([[],[],[],[]]) #TODO: Change this
-		self._gameState[self._X+1][self._Y].append(threat)
-	    if self._Y != (self._colLen - 1) and ('v' not in self._gameState[self._X][self._Y + 1]):            
-	        if len(self._gameState[self._X]) == (self._Y + 1):
-		    self._gameState[self._X].append([])
-		self._gameState[self._X][self._Y+1].append(threat)
-	    if self._X != 0 and ('v' not in self._gameState[self._X-1][self._Y]):
-		self._gameState[self._X-1][self._Y].append(threat)
-	    if self._Y != 0 and ('v' not in self._gameState[self._X][self._Y-1]):
-		self._gameState[self._X][self._Y-1].append(threat)
-	  		 
     # ======================================================================
 
     
